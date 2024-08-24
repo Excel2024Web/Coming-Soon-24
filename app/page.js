cc1f0a417a532dcd53ad75332a5e903875e45f89
Elizabeth <elizabethmariyashibu2004@gmail.com>
2024-08-24 15:31:00 +0530

    increased the size of logo, adjusted the legacy sizes ,changed the design of playbutton,changed the gradients of social media icon

--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-
 import Image from "next/image";
-
 import VideoDialog from './video';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Loader from './loader';  // Import the Loader component
 import LinkedInIcon from '@/public/icons/svg/linkedin'; 
 import FacebookIcon from '@/public/icons/svg/facebook';
 import IconButton from '@mui/material/IconButton';
 import InstagramIcon from '@/public/icons/svg/instagram';
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
 
 export default function Home() {
   const [open, setOpen] = useState(false);
@@ -57,7 +53,7 @@ export default function Home() {
 
       <div className="stars"></div>
       <div className="pl-10 z-10 flex flex-col -mt-36 sm:-mt-52 items-center justify-center relative">
-      <div id="evolution_in_progress" className="text-center text-white tracking-[0.1rem] sm:tracking-[0.5rem] scale-95 p-2 mb-2 text-base sm:text-lg">
+      <div id="evolution_in_progress" className="text-center text-white tracking-[0.1rem] sm:tracking-[0.5rem] scale-95 p-2 mr-5 -mt-4.8 -mb-0.5 text-base sm:text-lg">
           {applyFlickerEffect("EVOLUTION", flickerDelays)}
           <span>&nbsp;</span> {/* Space between words */}
           {applyFlickerEffect("IN", flickerDelays)}
@@ -65,17 +61,19 @@ export default function Home() {
           {applyFlickerEffect("PROGRESS", flickerDelays)}
         </div>
         <div id="excel_2024" className="expand">
-          <img
+          <Image
+          width={400}
+          height={250}
             src="/LOGONEW.png"  // Corrected path to the image in public folder
             alt="Excel 2024"
-            className="bigger w-32 h-16 sm:w-32 sm:h-16 lg:w-52 lg:h-20 tracking-custom-wide" // Adjust dimensions as needed
+            className="bigger -mt-1 mr-5" // Adjust dimensions as needed
             // border-0.125rem border-[#339DDC] shadow-[0_0_0.5rem_#3696CF]
           />
         </div>
         {/* video button */}
-        <div className="cursor-pointer video_btn absolute top-48 right-0.25 mb-10 flex items-center justify-center rounded-full transition-shadow duration-300 hover:bg-sky-600 hover:shadow-[0_40px_80px_rgba(59,130,246,0.6)] hover:shadow-blue-500/30">
+        <div className="cursor-pointer video_btn absolute top-48 right-0.25 mb-10 flex items-center justify-center  transition-shadow duration-300 hover:bg-sky-600 hover:shadow-[0_40px_80px_rgba(59,130,246,0.6)] hover:shadow-blue-500/30 transform scale-50">
           <div
-            className={`pop-up play relative w-20 h-20 bg-gradient-radial from-[rgb(102,207,255)] via-[#065c62] to-[#065c62] rounded-full flex items-center justify-center shadow-[0px_0.94rem_1.56rem_#8e9b9e66] transition-all duration-500 ${open ? 'shadow-[0px_0px_0px_150vh_var(--themeColour)]' : ''}`}
+            className={`mt-3 rounded-lg pop-up play relative w-24 h-24 bg-gradient-radial from-[rgb(133,102,255)] via-[#250662] to-[#590662] flex items-center justify-center shadow-[0px_0.94rem_1.56rem_#8e9b9e66] transition-all duration-500 ${open ? 'shadow-[0px_0px_0px_150vh_var(--themeColour)]' : ''}`}
             onClick={handleClick}
           >
             <div className="absolute inset-0 flex items-center justify-center">
@@ -83,20 +81,27 @@ export default function Home() {
             </div>
           </div>
         </div>
+
       </div>
 
       <div
         id="logos"
-        className="z-10 px-4 absolute bottom-4 flex flex-col sm:flex-row gap-y-2 items-center justify-between w-full "
+        className=" -translate-x-2 -translate-y-1 z-10 px-4 absolute bottom-4 flex flex-col sm:flex-row gap-y-2 items-center justify-between w-full "
       >
         <div id="social_logos" className="flex gap-1.5 justify-center">
         <a 
         href="https://facebook.com" 
         target="_blank" 
-        rel="noopener noreferrer" 
-        className="w-[50px] h-[50px] rounded-full flex items-center justify-center bg-gray-600 hover:bg-gray-400 transition-colors"
+       rel="noopener noreferrer"
+        className="w-12 h-12 rounded-full flex items-center justify-center  bg-gray-800    hover:bg-red-50 transition-colors"
       >
-        <FaFacebookF className="text-xl" style={{ color: 'white', textShadow: '6px 6px 5px rgb(0, 0, 0)' }} />
+         <Image
+          src='/icons/icons8-facebook.svg'
+          alt="Twitter"
+          width={20} // Adjust size as needed
+          height={20} // Adjust size as needed
+          className=" shadow-[8px_8px_8px_rgba(0,0,0,0.5)] hover:shadow-[6px_6px_12px_rgba(0,0,0,0.5)] transition-shadow"
+        />
       </a>
 
       <a
@@ -149,62 +154,84 @@ export default function Home() {
       </a>
     </div>
     <div id="legend_logos" className="flex">
-          <a href="https://2018.excelmec.org/" target="_blank">
+          <a href="https://2018.excelmec.org/" target="_blank"
+          className='mt-3 mr-2'>
             <Image
               src='/legacy/excel2018.png'
               alt="Excel 2018"
-              width={48}
-              height={48}
-              className="object-contain w-12 h-12 sm:w-16 sm:h-16"
+              width={40}
+              height={30}
+              
             />
           </a>
-          <a href="https://2019.excelmec.org/" target="_blank">
+          <a href="https://2019.excelmec.org/" target="_blank"
+          className='mt-3 mr-1'>
             <Image
                src='/legacy/excel2019.png'
               alt="Excel 2019"
-              width={48}
-              height={48}
-              className="object-contain w-12 h-12 sm:w-16 sm:h-16"
+              width={50}
+              height={45}
+             
             />
           </a>
-          <a href="https://2020.excelmec.org/" target="_blank">
+          <a href="https://2020.excelmec.org/" target="_blank"
+          className='mt-3 mr-3'>
             <Image
-             width={48}
-             height={48}
+             width={50}
+             height={45}
                src='/legacy/excel2020.png'
               alt="Excel 2020"
-              className="object-contain w-12 h-12 sm:w-16 sm:h-16"
+            
             />
           </a>
-          <a href="https://2021.excelmec.org/" target="_blank">
-            <Image
-              src='/legacy/excel2021.svg'
+       
+               <a 
+  href="https://2021.excelmec.org/"
+  target="_blank" 
+  className='mt-3 mr-3'
+   // Adjust padding
+>
+  <Image
+     src='/legacy/excel2021.svg'
               alt="Excel 2021"
-              width={48}
-              height={48}
-              className="object-contain w-12 h-12 sm:w-16 sm:h-16"
-            />
-          </a>
-          <a href="https://2022.excelmec.org/" target="_blank">
-            <Image
-              src='/legacy/excel2022.svg'
-              alt="Excel 2022"
-              width={48}
-              height={48}
-              className="object-contain w-12 h-12 sm:w-16 sm:h-16"
-            />
-          </a>
-          <a href="https://2023.excelmec.org/" target="_blank"> {/* Fixed link for 2023 */}
-            <Image
-              src='/legacy/excellogo-2023.png'
-              alt="Excel 2023"
-              width={48}
-              height={48}
-              className="object-contain w-12 h-12 sm:w-16 sm:h-16"
-            />
-          </a>
+    width={32}
+    height={30}
+   
+  />
+</a>
+          <a 
+  href="https://2022.excelmec.org/" 
+  target="_blank" 
+  className='mt-3 -mr-1'
+   // Adjust padding
+>
+  <Image
+    src="/legacy/excel2022.svg"
+    alt="Excel 2022"
+    width={40}
+    height={40}
+    lassName=" mt-2 object-contain w-12 h-12 sm:w-16 sm:h-16"
+  />
+</a>
+
+<a 
+  href="https://2023.excelmec.org/" 
+  target="_blank" 
+  className='mt-2'
+  // Adjust padding
+>
+  <Image
+    src="/legacy/excellogo-2023.png"
+    alt="Excel 2023"
+    width={58}
+    height={50}
+  />
+</a>
+
+
         </div>
       </div>
     </main>
   );
 }
+
