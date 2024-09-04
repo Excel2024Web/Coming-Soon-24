4ed2dab8b1f3bcfba48269bbe45ac6201108e322
Elizabeth <elizabethmariyashibu2004@gmail.com>
2024-09-04 19:04:15 +0530

    g-analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,41 @@
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head"; // Import Head component
+import Head from "next/head";
+import Script from "next/script"; // Import Script component
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Excel 2024",
   description:
-    "Coming Soon Website of Excel 2024, the annual national-level technical symposium of Govt. Model Engineering College, Thrikkakara.",
+    "Coming Soon Website of Excel 2024, the annual national-level technical symposium of Govt. Model Engineering College, Thrikkakara.",
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
         <link rel="shortcut icon" href="/icon.ico" />
-
-        {/* <link
-          rel="icon"
-          href="/favicon.png"  // Correct path for PNG favicon
-          sizes="any"
-          // type="image/png"
-        /> */}
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
       </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-QHQF8FR8MC"
+        />
+        <Script strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QHQF8FR8MC', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
